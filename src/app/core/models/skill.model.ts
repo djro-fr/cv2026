534fbc40c3d96b25b9f7bed972e08485d93d3de3
Sylvain Girault <syl@djro.fr>
2026-05-22 11:36:02 +0200

    feat: initialize app structure

---
+++ src/app/core/models/skill.model.ts
@@ -0,0 +1,8 @@
+export interface Skill {
+  id: number;
+  name : string;
+  category : string;
+  logo : string;
+  version : string | undefined;
+  level : number;
+}
---
+++ src/app/layouts/blank/blank.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blank',
+  imports: [],
+  templateUrl: './blank.html',
+  styleUrl: './blank.css',
+})
+export class Blank {}
