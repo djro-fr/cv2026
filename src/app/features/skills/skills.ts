14b2d577fd2841f26943ba50ec9670f9a14a100f
Sylvain Girault <syl@djro.fr>
2026-05-27 12:28:47 +0200

    refactor(skills): optimize template

--- src/app/features/skills/skills.ts
+++ src/app/features/skills/skills.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject, ViewChild } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SkillService } from '../../core/services/skill.service';
 import { AsyncPipe } from '@angular/common';
 
@@ -14,38 +14,21 @@ export class Skills {
   categories$ = this.skillService.getCategories();
   skills$ = this.skillService.getSkillsSortedByName();
 
-  catSelected : string = '';
-
-  @ViewChild('listCategories') listCategories!: ElementRef;
-  @ViewChild('categorySelected') categorySelected!: ElementRef;
-  @ViewChild('allCategoriesButton') allCategoriesButton!: ElementRef;
-  @ViewChild('allCategoriesTag') allCategoriesTag!: ElementRef;
+  categorySelected : string = '';
+  isCategoryListVisible = false;
 
   showAllSkills(){
-    const catElt = this.categorySelected.nativeElement;
-    const allCatsBtn = this.allCategoriesButton.nativeElement;
-    const allCatsTag = this.allCategoriesTag.nativeElement;
     this.skills$ = this.skillService.getSkillsSortedByName();
-    catElt.classList.replace('inline-block','hidden');
-    allCatsBtn.classList.replace('inline-block','hidden');
-    allCatsTag.classList.replace('hidden','inline-block');
+    this.categorySelected = '';
   }
 
   searchSkillsByCategory(cat:string){
-    const catElt = this.categorySelected.nativeElement;
-    const allCatsBtn = this.allCategoriesButton.nativeElement;
-    const allCatsTag = this.allCategoriesTag.nativeElement;
     this.skills$ = this.skillService.getSkillsByCategory(cat);
-    catElt.classList.replace('hidden','inline-block');
-    allCatsBtn.classList.replace('hidden','inline-block');
-    allCatsTag.classList.replace('inline-block','hidden');
-    this.catSelected = cat;
-
+    this.categorySelected = cat;
   }
 
   showCategories(){
-    const list = this.listCategories.nativeElement;
-    list.classList.toggle('hidden');
+    this.isCategoryListVisible = !this.isCategoryListVisible;
   }
 
 }
