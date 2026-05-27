e1b61be48035e67ceadf10901e64219e3a207db6
Sylvain Girault <syl@djro.fr>
2026-05-27 14:48:45 +0200

    feat(skills): add ornament

--- src/app/features/skills/skills.ts
+++ src/app/features/skills/skills.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { SkillService } from '../../core/services/skill.service';
 import { AsyncPipe } from '@angular/common';
+import { Observable, tap } from 'rxjs';
+import { SkillModel } from '../../core/models/skill.model';
 
 @Component({
   selector: 'app-skills',
@@ -10,21 +12,47 @@ import { AsyncPipe } from '@angular/common';
 })
 export class Skills {
   private readonly skillService = inject(SkillService);
+  readonly categories$ = this.skillService.getCategories();
 
-  categories$ = this.skillService.getCategories();
-  skills$ = this.skillService.getSkillsSortedByName();
+  showOrnament = true;
 
-  categorySelected : string = '';
+  private readonly cdr = inject(ChangeDetectorRef);
+
+  private resetOrnament() {
+    console.clear();
+    this.showOrnament = false;
+    setTimeout(() => {
+      this.showOrnament = true;
+      this.cdr.detectChanges();
+    }
+    , 10);
+  }
+
+  skillsCount = 0;
+
+  private pipeWithCount(o: Observable<SkillModel[]> ){
+    return o.pipe(
+      tap(skills => {
+        this.skillsCount = skills.length;
+      })
+    );
+  }
+
+  skills$ = this.pipeWithCount(this.skillService.getSkillsSortedByName())
+
+  categorySelected = '';
   isCategoryListVisible = false;
 
   showAllSkills(){
-    this.skills$ = this.skillService.getSkillsSortedByName();
+    this.skills$ = this.pipeWithCount(this.skillService.getSkillsSortedByName());
     this.categorySelected = '';
+    this.resetOrnament();
   }
 
   searchSkillsByCategory(cat:string){
-    this.skills$ = this.skillService.getSkillsByCategory(cat);
+    this.skills$ = this.pipeWithCount(this.skillService.getSkillsByCategory(cat));
     this.categorySelected = cat;
+    this.resetOrnament();
   }
 
   showCategories(){
