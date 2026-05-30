f5f27b5fd21a9dbced1d6b84e3e688e781a92e45
Sylvain Girault <syl@djro.fr>
2026-05-30 14:18:33 +0200

    feat(router): enable SSG prerendering for all routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,10 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'skills', renderMode: RenderMode.Prerender },
+  { path: 'experience', renderMode: RenderMode.Prerender },
+  { path: 'education', renderMode: RenderMode.Prerender },
+  { path: 'contact', renderMode: RenderMode.Prerender },
+  { path: '**', renderMode: RenderMode.Prerender }
 ];
